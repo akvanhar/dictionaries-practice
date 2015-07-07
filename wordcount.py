964f716379a93ecbc07e17de63d67d7d071c3b73
Hackbright Student <info@hackbrightacademy.com>
2015-07-06 17:20:14 -0700

    removed punctuation

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,23 @@
+import string
+
+def remove_punctuation(s):
+    s_no_punc = ""
+    for letter in s:
+        if letter not in string.punctuation:
+            s_no_punc += letter
+    return s_no_punc
+
 words_dict = {}
 
 word_file = open("twain.txt")
 
 for line in word_file:
 # iterate through each line in file
-    line = line.rstrip()
-    line = line.split()
+    line = line.rstrip().split()
 
     for word in line:
+        word = word.strip().lower()
+        word = remove_punctuation(word)
     # for every word in line
         if words_dict.get(word, 0) == 0:
         # if word is not in dictionary
